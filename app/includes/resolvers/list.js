c0476d51c808ce2ac413cf16fba93c98b6d00630
Dibran Isufi <d.isufi@gmail.com>
2017-10-24 18:40:21 +0200

    feat: resolve teaser list includes through liClient getDocumentList

--- app/includes/resolvers/list.js
+++ app/includes/resolvers/list.js
@@ -1,9 +1,16 @@
 async function resolveListIncludes (includes, liClient, renderListInclude) {
-  for (const include of includes) {
-    const content = include.getContent()
-    const html = renderListInclude(content)
-    include.resolve(html)
-  }
+  const proms = includes.map(include => {
+    const {params} = include.getContent()
+    const limit = params.count
+    // TODO: find out how to handle the offset (DI)
+    // const offset = params.offset
+    return liClient.getDocumentList({listId: params.listId, limit})
+      .then(documentList => {
+        const html = renderListInclude({...documentList, params})
+        include.resolve(html)
+      })
+  })
+  await Promise.all(proms)
 }
 
 module.exports = resolveListIncludes
--- app/includes/templates/list.js
+++ app/includes/templates/list.js
@@ -1,3 +1,44 @@
-module.exports = (content) => `
-${JSON.stringify(content, null, 2)}
+/* eslint max-len: 0 */
+
+module.exports = ({params, ...data}) => {
+  if (params.layout && params.layout === 'teaserRowHigh') return renderSkyscraperTeaserList(data)
+  return renderNormalTeaserList(data)
+}
+
+const renderSkyscraperTeaserList = ({documents}) => `
+<div class="teaser-row placeholder--teaser-row-high doc-component doc-component-highlight" doc-include="list" doc-component="teaser-row-high">
+${documents.map(({id, title, owner, metadata}) => `
+  <a
+    href="/articles/${id}"
+    class="teaser-row__item teaser-row__item--high teaser-row__item--bg"
+    style="background-image: url(${(metadata.teaserImage && metadata.teaserImage.url) || ''});"
+  >
+    <div class="teaser-row__inside">
+      <div class="teaser-row__headline">
+        <div class="teaser-row__headline-inner">${title || ''}</div>
+      </div>
+      <div class="teaser-author teaser-author--dark">
+        By <span class="teaser-author__name">${owner.last_name || ''}</span></div>
+    </div>
+  </a>
+`).join('')}
+</div>
+`
+
+const renderNormalTeaserList = ({documents}) => `
+<div class="teaser-row placeholder--teaser-row doc-component" doc-include="list" doc-component="teaser-row">
+${documents.map(({id, title, owner, metadata}) => `
+  <a href="/articles/${id}" class="teaser-row__item">
+    <div class="teaser-tag-wrap">
+      <img src="${(metadata.teaserImage && metadata.teaserImage.url) || ''}">
+    </div>
+    <div class="teaser-row__headline teaser-row__headline--outside">
+      <div class="teaser-row__headline-inner">${title || ''}</div>
+    </div>
+    <div class="teaser-author">
+      By <span class="teaser-author__name">${owner.last_name || ''}</span>
+    </div>
+  </a>
+`).join('')}
+</div>
 `
