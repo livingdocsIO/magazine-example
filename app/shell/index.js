677520c438445674aa9c4294a8f6e1a2ef294ffb
Dibran Isufi <d.isufi@gmail.com>
2017-11-15 13:43:15 +0100

    feat: handle arbitrary documentTypes via default layout

--- app/shell/index.js
+++ app/shell/index.js
@@ -1,9 +1,15 @@
-function render ({layout = 'page', menu = {}, location = '', documentHtml = '', ...rest}) {
+module.exports = function render ({
+  layout = 'default', menu = {}, location = '', documentHtml = '', ...rest
+}) {
   const renderShell = require('./main')
-  const renderLayout = require(`./layouts/${layout}`)
+  const renderLayout = require(`./layouts/${coerceToValidLayout(layout)}`)
 
   const shellContentHtml = renderLayout({...rest, menu, location, documentHtml})
   return renderShell({...rest, contentHtml: shellContentHtml})
 }
 
-module.exports = render
+function coerceToValidLayout (layout) {
+  const supportedLayouts = ['default', 'article', 'page']
+  if (supportedLayouts.includes(layout)) return layout
+  return 'default'
+}
