fdeb7762c6a4bc9ed8099e3d3d3fce1fb360844e
Dibran Isufi <d.isufi@gmail.com>
2017-11-23 10:09:32 +0100

    fix: dont ignore the secret sample

---
+++ conf/secrets/secret.sample.js
@@ -0,0 +1,14 @@
+/*
+ *  Private File
+ *  ------------
+ *
+ *  This file should be used to store secret configurations.
+ *  It must not be added to the repo.
+ *
+ *  Secrets override values in the environment config.
+ */
+module.exports = {
+  client: {
+    accessToken: 'your access token'
+  }
+}
