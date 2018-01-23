d58d09e8b955c9f74e1cb2e602c53abebbb7dea6
Dibran Isufi <d.isufi@gmail.com>
2018-01-23 12:56:18 +0100

    fix(design-build): use babel for ES6 transpiling
    uglify-js 2.x has known issues with ES6 features

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,14 @@ module.exports = {
   },
   module: {
     rules: [{
+      test: /\.js$/,
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015']
+        }
+      }]
+    }, {
       test: /\.(png|jpe?g|svg|gif)$/,
       loader: 'url-loader'
     }, {
