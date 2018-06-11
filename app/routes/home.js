850211541bf25d3c1f20ae3b248c15da43fb1b1b
Dibran Isufi <d.isufi@gmail.com>
2018-06-11 16:17:53 +0200

    fix: better error handling

--- app/routes/home.js
+++ app/routes/home.js
@@ -5,9 +5,15 @@ const {renderPage, renderError} = require('../rendering')
 module.exports = async (req, res) => {
   try {
     const headerMenuHandle = conf.get('navigation:headerMenuHandle')
-    const [publication] = await liClient.getPublications({homepage: true})
-    const [menu] = await liClient.getMenus({handle: headerMenuHandle})
 
+    const homePublications = await liClient.getPublications({homepage: true})
+    if (!homePublications || !homePublications.length) throw homepageNotFoundError()
+
+    const headerMenus = await liClient.getMenus({handle: headerMenuHandle})
+    if (!headerMenus || !headerMenus.length) throw headerMenuNotFoundError()
+
+    const [menu] = headerMenus
+    const [publication] = homePublications
     const html = await renderPage({
       menu,
       location: req.url,
@@ -24,4 +30,20 @@ module.exports = async (req, res) => {
     res.status(error.statusCode)
     return res.send(html)
   }
+}
+
+function homepageNotFoundError () {
+  const msg = 'Homepage not found'
+  return notFoundError(msg)
+}
+
+function headerMenuNotFoundError () {
+  const msg = 'Header menu not found'
+  return notFoundError(msg)
+}
+
+function notFoundError (msg) {
+  const error = new Error(msg)
+  error.statusCode = 404
+  return error
 }
--- app/routes/publications.js
+++ app/routes/publications.js
@@ -9,9 +9,17 @@ module.exports = async (req, res) => {
   try {
     const headerMenuHandle = conf.get('navigation:headerMenuHandle')
     const publication = await liClient.getPublication({documentId})
-    const [menu] = await liClient.getMenus({handle: headerMenuHandle})
+    if (!publication || publication.error) {
+      const error = publication && publication.error
+      const status = publication && publication.status
+      if (status === 404) throw publicationNotFoundError(documentId)
+      throw publicationRetrievalError(documentId, error, status)
+    }
 
+    const headerMenus = await liClient.getMenus({handle: headerMenuHandle})
+    if (!headerMenus || !headerMenus.length) throw headerMenuNotFoundError()
 
+    const [menu] = headerMenus
     const html = await renderPage({
       menu,
       location: req.url,
@@ -29,4 +37,27 @@ module.exports = async (req, res) => {
     res.status(error.statusCode)
     return res.send(html)
   }
+}
+
+function publicationRetrievalError (id, message, code = 500) {
+  const msg = `Could not retrieve publication with id "${id}" ${message || ''}`
+  const error = new Error(msg)
+  error.statusCode = code
+  return error
+}
+
+function publicationNotFoundError (id) {
+  const msg = `Publication with id "${id}" not found`
+  return notFoundError(msg)
+}
+
+function headerMenuNotFoundError () {
+  const msg = 'Header menu not found'
+  return notFoundError(msg)
+}
+
+function notFoundError (msg) {
+  const error = new Error(msg)
+  error.statusCode = 404
+  return error
 }
