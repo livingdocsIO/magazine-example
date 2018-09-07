e2840f717548f9a15b29145f93a8f81adf2015fe
Mehdi Avdi <mehdi.avdi@gmail.com>
2018-09-07 18:04:26 +0200

    feat(deploy-now-sh): add package.json script to deploy on now.sh

---
+++ conf/environments/now.js
@@ -0,0 +1,13 @@
+/* eslint-disable max-len */
+
+module.exports = {
+  client: {
+    url: 'http://localhost:3000',
+    accessToken: 'your access token'
+  },
+  imageServices: {
+    imgix: {
+      host: 'https://livingdocs-images.imgix.net'
+    }
+  }
+}
