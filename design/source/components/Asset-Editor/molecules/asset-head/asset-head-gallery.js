2a04675125adfc9687cf379c32790df9df6fc390
svenlivingdocs <sven@livingdocs.io>
2019-08-07 17:13:23 +0200

    design(asset editor): View options
    - Added

---
+++ design/source/components/Asset-Editor/molecules/asset-head/asset-head-gallery.js
@@ -0,0 +1,28 @@
+module.exports = (function () {
+
+  const assetHeadGallery = {
+    init () {
+      const pickerOptions = document.querySelectorAll('.a-asset-picker-option')
+      pickerOptions.forEach(function (pickerOption, index) {
+        pickerOption.addEventListener('click', event => {
+          const siblings = document.querySelectorAll('.a-asset-picker-option')
+          siblings.forEach(function (sibling) {
+            sibling.classList.remove('is-active')
+          })
+          pickerOption.classList.add('is-active')
+
+          const images = document.querySelectorAll('.m-asset-image')
+          images.forEach(function (image) {
+            if (index === 0) {
+              image.classList.add('m-asset-image--compact')
+            } else {
+              image.classList.remove('m-asset-image--compact')
+            }
+          })
+        })
+      })
+    }
+  }
+
+  return assetHeadGallery
+})()
