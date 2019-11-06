9af20d54e15134c96441f0c2ec35d94da2d3bf61
Gabriel <gabriel.hase@gmail.com>
2019-11-06 22:39:53 +0100

    fix(gallery): fix toggle

--- design/source/components/Asset-Editor/molecules/asset-head/asset-head-gallery.js
+++ design/source/components/Asset-Editor/molecules/asset-head/asset-head-gallery.js
@@ -1,30 +1,23 @@
-module.exports = (function () {
-
-  const assetHeadGallery = {
-    init () {
-      setTimeout(() => {
-        const pickerOptions = document.querySelectorAll('.a-asset-picker-option')
-        pickerOptions.forEach(function (pickerOption, index) {
-          pickerOption.addEventListener('click', event => {
-            const siblings = document.querySelectorAll('.a-asset-picker-option')
-            siblings.forEach(function (sibling) {
-              sibling.classList.remove('is-active')
-            })
-            pickerOption.classList.add('is-active')
-
-            const images = document.querySelectorAll('.m-asset-image')
-            images.forEach(function (image) {
-              if (index === 0) {
-                image.classList.add('m-asset-image--compact')
-              } else {
-                image.classList.remove('m-asset-image--compact')
-              }
-            })
-          })
+(function () {
+  setTimeout(() => {
+    const pickerOptions = document.querySelectorAll('.a-asset-picker-option')
+    pickerOptions.forEach(function (pickerOption, index) {
+      pickerOption.addEventListener('click', event => {
+        const siblings = document.querySelectorAll('.a-asset-picker-option')
+        siblings.forEach(function (sibling) {
+          sibling.classList.remove('is-active')
         })
-      }, 3000)
-    }
-  }
+        pickerOption.classList.add('is-active')
 
-  return assetHeadGallery
+        const images = document.querySelectorAll('.m-asset-image')
+        images.forEach(function (image) {
+          if (index === 0) {
+            image.classList.add('m-asset-image--compact')
+          } else {
+            image.classList.remove('m-asset-image--compact')
+          }
+        })
+      })
+    })
+  }, 3000)
 })()
